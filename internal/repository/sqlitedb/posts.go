9ea69f4e888ab05b5a8e049340c60cecd1acd3bb
alseiitov <dossan.alseiit@gmail.com>
2021-02-16 18:59:52 +0600

    use transaction with rollback to insert post to check that category really exists

--- internal/repository/sqlitedb/posts.go
+++ internal/repository/sqlitedb/posts.go
@@ -14,8 +14,35 @@ func NewPostsRepo(db *sql.DB) *PostsRepo {
 	return &PostsRepo{db: db}
 }
 
+// func (r *PostsRepo) Create(post model.Post) (int, error) {
+// 	stmt, err := r.db.Prepare("INSERT INTO posts (user_id, title, data, date, image) VALUES (?, ?, ?, ?, ?)")
+// 	if err != nil {
+// 		return 0, err
+// 	}
+// 	defer stmt.Close()
+
+// 	res, err := stmt.Exec(&post.UserID, &post.Title, &post.Data, &post.Date, &post.Image)
+// 	if err != nil {
+// 		return 0, err
+// 	}
+
+// 	id, err := res.LastInsertId()
+// 	if err != nil {
+// 		return 0, err
+// 	}
+
+// 	err = r.addCategoriesToPost(int(id), post.Categories)
+// 	return int(id), err
+// }
+
 func (r *PostsRepo) Create(post model.Post) (int, error) {
-	stmt, err := r.db.Prepare("INSERT INTO posts (user_id, title, data, date, image) VALUES (?, ?, ?, ?, ?)")
+	tx, err := r.db.Begin()
+
+	if err != nil {
+		return 0, err
+	}
+
+	stmt, err := tx.Prepare("INSERT INTO posts (user_id, title, data, date, image) VALUES (?, ?, ?, ?, ?)")
 	if err != nil {
 		return 0, err
 	}
@@ -28,28 +55,25 @@ func (r *PostsRepo) Create(post model.Post) (int, error) {
 
 	id, err := res.LastInsertId()
 	if err != nil {
+		tx.Rollback()
 		return 0, err
 	}
 
-	err = r.addCategoriesToPost(int(id), post.Categories)
-	return int(id), err
-}
-
-func (r *PostsRepo) addCategoriesToPost(postID int, categories []int) error {
-	for _, categorie := range categories {
-		stmt, err := r.db.Prepare("INSERT INTO posts_categories (post_id, categorie_id) VALUES (?, ?)")
+	for _, categorie := range post.Categories {
+		stmt, err := tx.Prepare("INSERT INTO posts_categories (post_id, categorie_id) VALUES (?, ?)")
 		if err != nil {
-			return err
+			tx.Rollback()
+			return 0, err
 		}
-		defer stmt.Close()
 
-		_, err = stmt.Exec(&postID, &categorie)
+		_, err = stmt.Exec(&id, &categorie)
 		if err != nil {
-			return err
+			tx.Rollback()
+			return 0, err
 		}
 	}
 
-	return nil
+	return int(id), tx.Commit()
 }
 
 func (r *PostsRepo) CreateComment(comment model.Comment) (int, error) {
