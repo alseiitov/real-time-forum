f7819eb97da70e36d36a26323512b7858dd05ff6
alseiitov <dossan.alseiit@gmail.com>
2021-03-15 13:10:16 +0600

    fix methods in clients main file

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -18,23 +18,23 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	temp, err := template.ParseFiles("./website/public/index.html")
+	temp, err := template.ParseFiles("./web/public/index.html")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	fileServer := http.FileServer(http.Dir("./website/src"))
+	fileServer := http.FileServer(http.Dir("./web/src"))
 
 	http.Handle("/src/", http.StripPrefix("/src/", fileServer))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		err := temp.Execute(w, conf.GetBackendAdress())
+		err := temp.Execute(w, conf.BackendAdress())
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
 		}
 	})
 
-	port := conf.GetFrontendPort()
+	port := conf.FrontendPort()
 	log.Printf("Frontend server is starting at %v", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatalln(err)
