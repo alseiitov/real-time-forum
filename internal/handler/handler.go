d74e1beb47eff1fe0bfef4510dd1ff04a79d1fa1
alseiitov <dossan.alseiit@gmail.com>
2021-05-16 15:10:26 +0600

    use route struct to init routes

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/alseiitov/gorouter"
@@ -10,6 +11,13 @@ import (
 	"github.com/alseiitov/real-time-forum/pkg/auth"
 	httpSwagger "github.com/swaggo/http-swagger"
 )
+
+type route struct {
+	Path    string
+	Method  string
+	MinRole int
+	Handler gorouter.Handler
+}
 
 type Handler struct {
 	Router               *gorouter.Router
@@ -38,111 +46,162 @@ func NewHandler(services *service.Services, tokenManager auth.TokenManager) *Han
 }
 
 func (h *Handler) Init() {
-	h.initUsersHandlers()
-	h.initPostsHandlers()
-	h.initCategoriesHandlers()
-	h.initCommentsHandlers()
-	h.initAdminsHandlers()
-	h.initChatHandlers()
-
-	h.initSwaggerHandler()
-	h.initImagesFileServer()
-
-}
-
-func (h *Handler) initUsersHandlers() {
-	r := h.Router
-
-	r.POST("/api/users/sign-up",
-		h.cors(h.identify(model.Roles.Guest, h.usersSignUp)))
-
-	r.POST("/api/users/sign-in",
-		h.cors(h.identify(model.Roles.Guest, h.usersSignIn)))
-
-	r.GET("/api/users/:user_id",
-		h.cors(h.identify(model.Roles.Guest, h.getUser)))
-
-	r.POST("/api/auth/refresh",
-		h.cors(h.usersRefreshTokens))
-
-	r.POST("/api/moderators/requests",
-		h.cors(h.identify(model.Roles.User, h.requestModerator)))
-
-	r.GET("/api/notifications",
-		h.cors(h.identify(model.Roles.User, h.getNotifications)))
-}
-
-func (h *Handler) initPostsHandlers() {
-	r := h.Router
-
-	r.GET("/api/posts/:post_id",
-		h.cors(h.identify(model.Roles.Guest, h.getPost)))
-
-	r.POST("/api/posts",
-		h.cors(h.identify(model.Roles.User, h.createPost)))
-
-	r.DELETE("/api/posts/:post_id",
-		h.cors(h.identify(model.Roles.User, h.deletePost)))
-
-	r.POST("/api/posts/:post_id/likes",
-		h.cors(h.identify(model.Roles.User, h.likePost)))
-}
-
-func (h *Handler) initCategoriesHandlers() {
-	r := h.Router
-
-	r.GET("/api/categories",
-		h.cors(h.identify(model.Roles.Guest, h.getAllCategories)))
-
-	r.GET("/api/categories/:category_id/:page",
-		h.cors(h.identify(model.Roles.Guest, h.getCategoryPage)))
-}
-
-func (h *Handler) initCommentsHandlers() {
-	r := h.Router
-
-	r.GET("/api/posts/:post_id/comments/:page",
-		h.cors(h.identify(model.Roles.Guest, h.getCommentsOfPost)))
-
-	r.POST("/api/posts/:post_id/comments",
-		h.cors(h.identify(model.Roles.User, h.createComment)))
-
-	r.POST("/api/comments/:comment_id/likes",
-		h.cors(h.identify(model.Roles.User, h.likeComment)))
-
-	r.DELETE("/api/comments/:comment_id",
-		h.cors(h.identify(model.Roles.User, h.deleteComment)))
-}
-
-func (h *Handler) initAdminsHandlers() {
-	r := h.Router
-
-	r.GET("/api/moderators/requests",
-		h.cors(h.identify(model.Roles.Admin, h.getRequestsForModerator)))
-
-	r.POST("/api/moderators/requests/:request_id",
-		h.cors(h.identify(model.Roles.Admin, h.RequestForModeratorAction)))
-}
-
-func (h *Handler) initChatHandlers() {
-	r := h.Router
-
-	r.GET("/ws", h.identify(model.Roles.User, h.handleWebSocket))
-
-}
-
-func (h *Handler) initSwaggerHandler() {
-	r := h.Router
-
-	r.GET("/swagger/*",
-		gorouter.WrapHandler(httpSwagger.Handler(
-			httpSwagger.URL("http://localhost:8081/swagger/doc.json"))))
-}
-
-func (h *Handler) initImagesFileServer() {
-	r := h.Router
 	images := http.FileServer(http.Dir("./database/images"))
 
-	r.GET("/images/*",
-		gorouter.WrapHandler(http.StripPrefix("/images/", images)))
+	routes := []route{
+		// User handlers
+		route{
+			Path:    "/api/users/sign-up",
+			Method:  "POST",
+			MinRole: model.Roles.Guest,
+			Handler: h.usersSignUp,
+		},
+		route{
+			Path:    "/api/users/sign-in",
+			Method:  "POST",
+			MinRole: model.Roles.Guest,
+			Handler: h.usersSignIn,
+		},
+		route{
+			Path:    "/api/users/:user_id",
+			Method:  "GET",
+			MinRole: model.Roles.Guest,
+			Handler: h.getUser,
+		},
+		route{
+			Path:    "/api/auth/refresh",
+			Method:  "POST",
+			MinRole: model.Roles.Guest,
+			Handler: h.usersRefreshTokens,
+		},
+		route{
+			Path:    "/api/moderators/requests",
+			Method:  "POST",
+			MinRole: model.Roles.User,
+			Handler: h.requestModerator,
+		},
+		route{
+			Path:    "/api/notifications",
+			Method:  "GET",
+			MinRole: model.Roles.User,
+			Handler: h.getNotifications,
+		},
+
+		// Post handlers
+		route{
+			Path:    "/api/posts/:post_id",
+			Method:  "GET",
+			MinRole: model.Roles.Guest,
+			Handler: h.getPost,
+		},
+		route{
+			Path:    "/api/posts",
+			Method:  "POST",
+			MinRole: model.Roles.User,
+			Handler: h.createPost,
+		},
+		route{
+			Path:    "/api/posts/:post_id",
+			Method:  "DELETE",
+			MinRole: model.Roles.User,
+			Handler: h.deletePost,
+		},
+		route{
+			Path:    "/api/posts/:post_id/likes",
+			Method:  "POST",
+			MinRole: model.Roles.User,
+			Handler: h.likePost,
+		},
+
+		// Categories handlers
+		route{
+			Path:    "/api/categories",
+			Method:  "GET",
+			MinRole: model.Roles.Guest,
+			Handler: h.getAllCategories,
+		},
+		route{
+			Path:    "/api/categories/:category_id/:page",
+			Method:  "GET",
+			MinRole: model.Roles.Guest,
+			Handler: h.getCategoryPage,
+		},
+
+		// Comments Handlers
+		route{
+			Path:    "/api/posts/:post_id/comments/:page",
+			Method:  "GET",
+			MinRole: model.Roles.Guest,
+			Handler: h.getCommentsOfPost,
+		},
+		route{
+			Path:    "/api/posts/:post_id/comments",
+			Method:  "POST",
+			MinRole: model.Roles.User,
+			Handler: h.createComment,
+		},
+		route{
+			Path:    "/api/comments/:comment_id/likes",
+			Method:  "POST",
+			MinRole: model.Roles.User,
+			Handler: h.likeComment,
+		},
+		route{
+			Path:    "/api/comments/:comment_id",
+			Method:  "DELETE",
+			MinRole: model.Roles.User,
+			Handler: h.deleteComment,
+		},
+
+		// Admins Handlers
+		route{
+			Path:    "/api/moderators/requests",
+			Method:  "GET",
+			MinRole: model.Roles.Admin,
+			Handler: h.getRequestsForModerator,
+		},
+		route{
+			Path:    "/api/moderators/requests/:request_id",
+			Method:  "POST",
+			MinRole: model.Roles.Admin,
+			Handler: h.RequestForModeratorAction,
+		},
+
+		// Chat Handlers
+		route{
+			Path:    "/ws",
+			Method:  "GET",
+			MinRole: model.Roles.User,
+			Handler: h.handleWebSocket,
+		},
+
+		// Swagger handler
+		route{
+			Path:    "/swagger/*",
+			Method:  "GET",
+			MinRole: model.Roles.Guest,
+			Handler: gorouter.WrapHandler(httpSwagger.Handler(httpSwagger.URL("http://localhost:8081/swagger/doc.json"))),
+		},
+
+		// Images filserver
+		route{
+			Path:    "/images/*",
+			Method:  "GET",
+			MinRole: model.Roles.Guest,
+			Handler: gorouter.WrapHandler(http.StripPrefix("/images/", images)),
+		},
+	}
+
+	for _, route := range routes {
+		switch route.Method {
+		case "GET":
+			h.Router.GET(route.Path, h.cors(h.identify(route.MinRole, route.Handler)))
+		case "POST":
+			h.Router.POST(route.Path, h.cors(h.identify(route.MinRole, route.Handler)))
+		case "DELETE":
+			h.Router.DELETE(route.Path, h.cors(h.identify(route.MinRole, route.Handler)))
+		default:
+			log.Fatalf("error: invalid method \"%v\" for route \"%v\"", route.Method, route.Path)
+		}
+	}
 }
