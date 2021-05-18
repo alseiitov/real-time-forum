9d62c7db174bf8fd65cec97f43e2b2a003f490b7
alseiitov <dossan.alseiit@gmail.com>
2021-05-18 16:03:14 +0600

    add auth for websocket connections

--- internal/handler/websockets.go
+++ internal/handler/websockets.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/alseiitov/gorouter"
@@ -16,11 +17,11 @@ import (
 var clients = make(map[int]*client)
 
 const (
+	tokenWait      = 10 * time.Second
 	writeWait      = 10 * time.Second
 	pongWait       = 60 * time.Second
 	pingPeriod     = (pongWait * 9) / 10
 	maxMessageSize = 2048
-	tokenWait      = 60 * time.Second
 )
 
 var upgrader = websocket.Upgrader{
@@ -32,74 +33,92 @@ var upgrader = websocket.Upgrader{
 type client struct {
 	id        int
 	conn      *websocket.Conn
+	mu        sync.Mutex
 	broadcast chan *WSEvent
 }
 
 type WSEvent struct {
 	Type string      `json:"type"`
-	Body interface{} `json:"body"`
+	Body interface{} `json:"body,omitempty"`
 }
 
 func (h *Handler) clientReadPump(c *client) {
-	defer func() {
-		c.conn.Close()
-		delete(clients, c.id)
-	}()
+	ticker := time.NewTicker(tokenWait)
+	defer ticker.Stop()
+
 	c.conn.SetReadLimit(maxMessageSize)
 	c.conn.SetReadDeadline(time.Now().Add(pongWait))
-	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
-	for {
-		_, messageBytes, err := c.conn.ReadMessage()
-		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Printf("error: %v", err)
-			}
-			break
-		}
 
-		var event WSEvent
-		err = json.Unmarshal(messageBytes, &event)
-		if err != nil {
-			if err := c.conn.WriteJSON(&WSEvent{Type: "error", Body: err.Error()}); err != nil {
-				log.Println(err)
+	go func() {
+		defer func() {
+			c.conn.Close()
+			delete(clients, c.id)
+		}()
+		for {
+			_, messageBytes, err := c.conn.ReadMessage()
+			if err != nil {
+				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+					log.Printf("error: %v", err)
+				}
 				return
 			}
-			return
-		}
 
-		switch event.Type {
-		case "message":
-			if c.id != 0 {
-				msgText := strings.TrimSpace(strings.Replace(fmt.Sprintf("%s", event.Body), "\n", " ", -1))
-				if len(msgText) > 0 {
-					body := model.Message{
-						Message: msgText,
-						Date:    time.Now(),
-						UserID:  c.id,
-						Read:    false,
-					}
-					c.broadcast <- &WSEvent{Type: "message", Body: body}
-				}
-			}
-		case "token":
-			token := fmt.Sprintf("%s", event.Body)
-			sub, _, err := h.tokenManager.Parse(token)
+			var event WSEvent
+			err = json.Unmarshal(messageBytes, &event)
 			if err != nil {
-				if err := c.conn.WriteJSON(&WSEvent{Type: "error", Body: err.Error()}); err != nil {
-					log.Println(err)
+				c.writeJSON(&WSEvent{Type: "error", Body: err.Error()})
+				return
+			}
+
+			switch event.Type {
+			case "message":
+				if c.id != 0 {
+					msgText := strings.TrimSpace(strings.Replace(fmt.Sprintf("%s", event.Body), "\n", " ", -1))
+					if len(msgText) > 0 {
+						body := model.Message{
+							Message: msgText,
+							Date:    time.Now(),
+							UserID:  c.id,
+							Read:    false,
+						}
+						c.broadcast <- &WSEvent{Type: "message", Body: body}
+					}
+				}
+			case "token":
+				token := fmt.Sprintf("%s", event.Body)
+				sub, _, err := h.tokenManager.Parse(token)
+				if err != nil {
+					c.writeJSON(&WSEvent{Type: "error", Body: err.Error()})
 					return
 				}
-				return
-			}
-			c.id = sub
-			clients[c.id] = c
-		default:
-			if err := c.conn.WriteJSON(&WSEvent{Type: "error", Body: "invalid event type"}); err != nil {
-				log.Println(err)
+				c.id = sub
+				clients[c.id] = c
+			case "pongMessage":
+				c.conn.SetReadDeadline(time.Now().Add(pongWait))
+			default:
+				c.writeJSON(&WSEvent{Type: "error", Body: "invalid event type"})
 				return
 			}
 		}
+	}()
+
+	<-ticker.C
+	if c.id == 0 {
+		c.writeJSON(&WSEvent{Type: "error", Body: "no token received"})
+		c.conn.Close()
+		delete(clients, c.id)
 	}
+}
+
+func (c *client) writeJSON(data interface{}) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	err := c.conn.WriteJSON(data)
+	if err != nil {
+		log.Println(err)
+	}
+	return err
 }
 
 func (h *Handler) clientWritePump(c *client) {
@@ -113,26 +132,24 @@ func (h *Handler) clientWritePump(c *client) {
 		case event, ok := <-c.broadcast:
 			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
 			if !ok {
-				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				c.writeJSON(&WSEvent{Type: "closeMessage"})
 				return
 			}
 
-			if err := c.conn.WriteJSON(&event); err != nil {
-				log.Println(err)
+			if err := c.writeJSON(&event); err != nil {
 				return
 			}
-
 			// Add queued chat messages to the current websocket message.
 			n := len(c.broadcast)
 			for i := 0; i < n; i++ {
-				if err := c.conn.WriteJSON(<-c.broadcast); err != nil {
-					log.Println(err)
+				if err := c.writeJSON(<-c.broadcast); err != nil {
 					return
 				}
 			}
 		case <-ticker.C:
 			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
-			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+
+			if err := c.writeJSON(&WSEvent{Type: "pingMessage"}); err != nil {
 				return
 			}
 		}
@@ -152,10 +169,10 @@ func (h *Handler) handleWebSocket(ctx *gorouter.Context) {
 	go h.clientReadPump(c)
 	go h.clientWritePump(c)
 
-	go func() {
-		for {
-			fmt.Println(clients)
-			time.Sleep(5 * time.Second)
-		}
-	}()
+	// go func() {
+	// 	for {
+	// 		fmt.Println(clients)
+	// 		time.Sleep(5 * time.Second)
+	// 	}
+	// }()
 }
